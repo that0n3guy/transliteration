1a8c096993cf79c403fea8ab0498f52ab31ba0a6
unknown <peterolson@simplistechsolutions.com>
2014-04-14 16:32:58 -0500

    initial commit

---
+++ src/That0n3guy/Transliteration/Facades/Transliteration.php
@@ -0,0 +1,14 @@
+<?php namespace That0n3guy\Transliteration\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Transliteration extends Facade {
+
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor() { return 'Transliteration'; }
+
+}
